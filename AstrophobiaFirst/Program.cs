2494d9c85e5064f54ecec4f47677b1338c144b72
Saskia Brienesse-little <saskiablittle@gmail.com>
2024-05-25 10:52:40 +1200

    Look code improvment

--- AstrophobiaFirst/Program.cs
+++ AstrophobiaFirst/Program.cs
@@ -47,38 +47,65 @@
         {
             Console.WriteLine("How we move around the ship");
             int y, n, inventory, menu, look, leave;
-            
+
         }
-        static void Look()
+        static void Look(ref int count)
         {
             Console.WriteLine("You have looked around the room");
             Console.ReadLine();
-            int count = 0;
-            if (count == 0)
-            {
-                Console.WriteLine("you see a torch on the ground, do you pick it up?");
-                count++;
-
-            }
             if (count == 1)
             {
-                Console.WriteLine("HI");
+                string temp;
+                do
+                {
+                    Console.WriteLine("you see a torch on the ground, do you pick it up? y or n");
+                    temp = Console.ReadLine();
+                    switch (temp)
+                    {
+                        case "y":
+                            Console.WriteLine("You have picked up the tourch and turn it on...\n");
+                            Console.ReadLine();
+                            break;
+                        case "n":
+                            Console.WriteLine("You desided not to pick up the tourch, but you still can't see.\nMaybe it would be better to pick it up.");
+                            break;
+                    }
+                } while (temp != "y");
+
             }
+            if (count == 2)
+            {
+                Console.WriteLine("There is nothing else in the room");
+            }
+
         }
         public static void Dorm()
         {
+            int count = 0;
             Console.WriteLine("You awaken in the dorm and it is dark. Maybe there is something in the room to help you see better.\nWhat would you like to do, your options are:\nlook\nleave");
             string temp = Console.ReadLine();
             switch (temp)
             {
                 case "look":
-                    Look();
+                    count++;
+                    Look(ref count);
                     break;
                 case "leave":
                     Hall();
                     break;
             }
-            
+            Console.WriteLine("You can now see around the room. \nThere are many beds but you seem to be the only one here. \nAre you alone? \nMaybe you will find answers if you explore outside of the room, through the door in front of you...");
+            string temp1 = Console.ReadLine();
+            switch (temp1)
+            {
+                case "leave":
+                    Hall();
+                    break;
+                case "look":
+                    count++;
+                    Look(ref count);
+                    break;
+            }
         }
         static void Med()
         {
